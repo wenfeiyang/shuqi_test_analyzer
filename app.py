7c3be49cb62dd8055d9ba1735097a04dcbe3067f
wenfeiyang <xiaoxiaowenzi@gmail.com>
2015-03-10 19:35:12 +0800

    fix statistic function

--- app.py
+++ app.py
@@ -25,40 +25,39 @@ def get_test_statistic():
     device = request.args.get('device')
     if device:
         criteria.update(device_name=device)
-    appium = request.args.get('appium')
-    if appium:
-        criteria.update(appium_version=appium)
-    collection = app.data.driver.db["test_result"]
-    tests = collection.distinct("test_name")
-    print tests
+    else:
+        device = "All devices"
+    version = request.args.get('ver')
+    if version:
+        criteria.update(app_version=version)
+    else:
+        version = "All versions"
+
+    test_set = app.data.driver.db["test_map"].find()
+    find_func = app.data.driver.db["test_result"].find
     result = {}
     overall = {}
-    for test in tests:
-        print test
-        m = re.match(r'.*\((.*)\)', test)
-        name = m.group(1).rsplit('.', 1)[0] if m else test
-        sta_dict = {"name": name, "pass": 0, "fail": 0, "error": 0}
-        if name in result:
-            sta_dict = result[name]
-        else:
-            result[name] = sta_dict
-        query = criteria
-        query.update(test_name=test)
-        query.update(status=0)
-        sta_dict["pass"] += collection.find(query).count()
-        query.update(status=1)
-        sta_dict["fail"] += collection.find(query).count()
-        query.update(status=2)
-        sta_dict["error"] += collection.find(query).count()
-        overall["name"] = "overall"
-        query = criteria
-        query.update(status=0)
-        overall["pass"] =  collection.find(query).count()
-        query.update(status=1)
-        overall["fail"] =  collection.find(query).count()
-        query.update(status=2)
-        overall["error"] =  collection.find(query).count()
-        return render_template("test_statistic.html", result=result, overall=overall)
+    for test in test_set:
+        id = test['test_id']
+        test_id = int(id)
+        sta_dict = {"id": test_id, "description": test['test_description'], "pass": 0, "fail": 0, "error": 0}
+        query = criteria.copy()
+        query.update(test_id=id, status=0)
+        sta_dict["pass"] = find_func(query).count()
+        query.update(test_id=id, status=1)
+        sta_dict["fail"] = find_func(query).count()
+        query.update(test_id=id, status=2)
+        sta_dict["error"] = find_func(query).count()
+        result[test_id] = sta_dict
+    overall["description"] = u"总体结果"
+    query = criteria.copy()
+    query.update(status=0)
+    overall["pass"] = find_func(query).count()
+    query.update(status=1)
+    overall["fail"] = find_func(query).count()
+    query.update(status=2)
+    overall["error"] = find_func(query).count()
+    return render_template("test_statistic.html", device=device, version=version, result=result, overall=overall)
 
 
 @app.route("/tests/failure")
@@ -68,34 +67,6 @@ def failure_tests():
     )
     return render_template("failure_tests.html", records=records)
 
-@app.route("/tests/statistic")
-def tests_statistic():
-	test_set = app.data.driver.db["test_result"].distinct("test_name")
-	result = {}
-	overall = {}
-	find_func = app.data.driver.db["test_result"].find
-	for test in test_set:
-		m = re.match(r'.*\((.*)\)', test)
-		name = m.group(1).rsplit('.',1)[0] if m else test
-		sta_dict = {"name": name, "pass": 0, "fail": 0, "error": 0}
-		if name in result:
-			sta_dict = result[name]
-		else:
-			result[name] = sta_dict
-		sta_dict["pass"] += find_func(
-			{"test_name": test, "status": 0}
-		).count()
-		sta_dict["fail"] += find_func(
-			{"test_name": test, "status": 1}
-		).count()
-		sta_dict["error"] += find_func(
-			{"test_name": test, "status": 2}
-		).count()
-	overall["name"] = "overall"
-	overall["pass"] = find_func({"status": 0}).count()
-	overall["fail"] = find_func({"status": 1}).count()
-	overall["error"] = find_func({"status": 2}).count()
- 	return render_template("test_statistic.html", result=result, overall=overall)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='10.3.86.107')
